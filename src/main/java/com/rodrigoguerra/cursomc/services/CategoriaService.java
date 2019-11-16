ec9876691b486eff4f7145b5260b00ae63f242b6
Rodrigo Guerra <rodrigoguerra.rj@gmail.com>
2019-11-16 17:50:09 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/rodrigoguerra/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigoguerra/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigoguerra.cursomc.domain.Categoria;
+import com.rodrigoguerra.cursomc.dto.CategoriaDTO;
 import com.rodrigoguerra.cursomc.repositories.CategoriaRepository;
 import com.rodrigoguerra.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigoguerra.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome()); 
 	}
 }
